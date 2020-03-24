a077b7ad9b0f8a73dd23e43bcdfbf538fae89c71
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-24 18:50:27 +0200

    App: Replaces logo image with agents image, changes texts
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import image from './agents.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Codenames</code>
         </p>
+        <img src={image} className="image-logo" alt="logo" />
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          New Game
         </a>
       </header>
     </div>
