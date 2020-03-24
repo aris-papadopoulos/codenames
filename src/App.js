23cf1aa6fb794f5832cc917c19e236f7ae8e53e0
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-24 23:07:30 +0200

    App: Adds function for randomly picking cards for game initiation
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/App.js
+++ src/App.js
@@ -14,12 +14,30 @@ function App() {
         <button className="create-game" onClick={createGame}>
           New Game
         </button>
-        <button className="create-map" onClick={createMap}>
+        <button className="create-map" onClick={null}>
           Create Map
         </button>
       </header>
     </div>
   );
+}
+
+// Randomly picks 25 cards to start a new game
+function createGame() {
+    const gameCards = pickCards();
+
+    console.log(gameCards);
+}
+
+function pickCards() {
+    let gameCards = [];
+    while (gameCards.length < 24) {
+        const card = Math.floor(Math.random() * cards.length);
+        if (!gameCards.includes(card)) {
+            gameCards.push(card);
+        }
+    }
+    return gameCards;
 }
 
 export default App;
