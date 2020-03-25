91cdcb6a55d742cc7a54d7f8b96d9acc14ad9d03
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-25 13:56:36 +0200

    Creates dummy "Game" & "NotFound" component for corresponding pages
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

---
+++ src/Game.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { cards } from './cards';
+// import './game.scss';
+
+function Game() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <code>Game</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Game;
---
+++ src/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Page not found
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default NotFound;
