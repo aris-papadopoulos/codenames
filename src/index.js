ac8f938eaf9130885f44e0168a942ef063333579
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-25 13:58:36 +0200

    Sets up application routes
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import Game from './Game';
+import NotFound from './NotFound';
+import { Router, Route, Switch } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
+// Needed for passing history object and create redirects within redux actions
+import { createBrowserHistory } from 'history';
+
+const history =  createBrowserHistory();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router history={history}>
+            <Switch>
+                <Route exact path="/game/:id" component={Game} />
+                <Route exact path="/" component={App} />
+                <Route path="*" component={NotFound} />
+            </Switch>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
