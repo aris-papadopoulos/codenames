68f15e8a5f3a98bff45770ffa1ac439c7afb8f7e
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-26 11:00:58 +0200

    Formatting changes (LF / spaces etc) for utilities & NotFound files
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 function NotFound() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Page not found
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Page not found</p>
+            </header>
+        </div>
+    );
 }
 
 export default NotFound;
--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,3 @@
-
 // Needed for passing history object and create redirects within redux actions
 import { createBrowserHistory } from 'history';
 export default createBrowserHistory();
