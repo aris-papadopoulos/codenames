e1c9bc86d3e163bae2307108650d6c3d667de4fa
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-26 16:38:15 +0200

    Key: Adds createNewKey func & form with create/insert buttons
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Key.js
+++ src/Key.js
@@ -1,29 +1,85 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './key.scss';
 
-const Key = (props) => {
+const Key = () => {
+    
+    const [key, setKey] = useState({encoded: ''});
+
+    useEffect(() => {
+        if (!key.array) {
+            createKey();
+        }
+    }, [key, setKey]);
+
+    const createKey = () => {
+        let key = createNewKey();
+        console.log(key);
+
+        var myJSON = JSON.stringify(key);
+        const encoded = window.btoa(myJSON);
+        key = {...key, encoded}
+
+        console.log(key);
+
+        setKey(key);
+    }
+
     return (
-        <div className="key">
-            <div className="key__outer">
-                <div className="key__inner">
-                    {(props.data) ?
-                        props.data.map((el, i) => {
-                            let classValue;
-                            if      (el === 1) { classValue = 'blue' }
-                            else if (el === 2) { classValue = 'red' }
-                            else if (el === 3) { classValue = 'neutral' }
-                            else if (el === 4) { classValue = 'executor' }
-                        return <span key={i} className={`key-element ${classValue}`}></span>
-                    })
-                    : null}
-                        <span className={`indicator ${props.initiator} top`}></span>
-                        <span className={`indicator ${props.initiator} bottom`}></span>
-                        <span className={`indicator ${props.initiator} left`}></span>
-                        <span className={`indicator ${props.initiator} right`}></span>
+        <div className="key-wrapper">
+            <div className="key">
+                <div className="key__outer">
+                    <div className="key__inner">
+                        {(key.array) ?
+                            key.array.map((el, i) => {
+                                let classValue;
+                                if      (el === 1) { classValue = 'blue' }
+                                else if (el === 2) { classValue = 'red' }
+                                else if (el === 3) { classValue = 'neutral' }
+                                else if (el === 4) { classValue = 'executor' }
+                            return <span key={i} className={`key-element ${classValue}`}></span>
+                        })
+                        : null}
+                            <span className={`indicator ${key.init} top`}></span>
+                            <span className={`indicator ${key.init} bottom`}></span>
+                            <span className={`indicator ${key.init} left`}></span>
+                            <span className={`indicator ${key.init} right`}></span>
+                    </div>
                 </div>
+            </div>
+            <div className="key-form">
+                <button onClick={() => createKey()}>Δημιουργία</button>
+                <button>Εισαγωγή</button>
+                <input type="text" className="key__code" defaultValue={key.encoded} onChange={null} />
             </div>
         </div>
     );
+}
+
+function createNewKey() {
+    // Define which team starts
+    let  keyArray = [];
+    const initNumber = Math.floor(Math.random() * 2);
+    const initiator = (initNumber) ? 'blue' : 'red';
+
+    // Create array of elements
+    // "arr" has 8 blue cards, 8 red, 7 neutral and 1 executor card. 
+    let elemsArray = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4];
+    // The last card depends on which team starts first (team who starts first has 1 more card)
+    (initiator === 'blue') ? elemsArray.push(1) : elemsArray.push(2);
+
+    // Create random map array
+    while (keyArray.length < 25) {
+        const index = Math.floor(Math.random() * elemsArray.length);
+        keyArray.push(elemsArray[index]);
+        elemsArray.splice(index, 1);
+    }
+
+    const keyObject = {
+        array: keyArray,
+        init: initiator
+    }
+
+    return keyObject;
 }
 
 export default Key;
