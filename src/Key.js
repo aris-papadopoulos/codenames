c361cf91e6be9d2adc34f46f79d7b339f41988b6
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-26 13:03:24 +0200

    Key: Adds visuals & team init indicator
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/Key.js
+++ src/Key.js
@@ -2,13 +2,28 @@ import React from 'react';
 import './key.scss';
 
 const Key = (props) => {
-  return (
-    <div className="key">
-        {(props.data) ?
-        props.data.map((el, i) => <span key={i}>{el}</span>)
-        : null}
-    </div>
-  );
+    return (
+        <div className="key">
+            <div className="key__outer">
+                <div className="key__inner">
+                    {(props.data) ?
+                        props.data.map((el, i) => {
+                            let classValue;
+                            if      (el === 1) { classValue = 'blue' }
+                            else if (el === 2) { classValue = 'red' }
+                            else if (el === 3) { classValue = 'neutral' }
+                            else if (el === 4) { classValue = 'executor' }
+                        return <span key={i} className={`key-element ${classValue}`}></span>
+                    })
+                    : null}
+                        <span className={`indicator ${props.initiator} top`}></span>
+                        <span className={`indicator ${props.initiator} bottom`}></span>
+                        <span className={`indicator ${props.initiator} left`}></span>
+                        <span className={`indicator ${props.initiator} right`}></span>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Key;
