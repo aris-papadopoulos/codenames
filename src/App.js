08b2aded342f5067275e89d0e734fc1c4ebcdd57
Aris Papadopoulos <aris.ppd@gmail.com>
2020-03-26 09:53:00 +0200

    App: Removes createKey function which is no longer needed here
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/App.js
+++ src/App.js
@@ -23,7 +23,6 @@ function App() {
 // Randomly picks 25 cards to start a new game
 function createGame() {
     const cards = pickCards();
-    const map = createMap();
 
     var myJSON = JSON.stringify(cards);
     const encodedGame = window.btoa(myJSON);
@@ -43,27 +42,5 @@ function pickCards() {
     return gameCards;
 }
 
-function createMap() {
-    // Define which team starts
-    let  mapArray = [];
-    const initNumber = Math.floor(Math.random() * 2);
-    const initiator = (initNumber) ? 'blue' : 'red';
-
-    // Create array of elements
-    // "arr" has 8 blue cards, 8 red, 7 neutral and 1 executor card. 
-    let elemsArray = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 4];
-    // The last card depends on which team starts first (team who starts first has 1 more card)
-    (initiator === 'blue') ? elemsArray.push(1) : elemsArray.push(2)
-    console.log(initiator);
-
-    // Create random map array
-    while (mapArray.length < 25) {
-        const index = Math.floor(Math.random() * elemsArray.length);
-        mapArray.push(elemsArray[index]);
-        elemsArray.splice(index, 1);
-    }
-
-    return mapArray;
-}
 
 export default App;
