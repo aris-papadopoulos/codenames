dcc2f31d18a2f0997d87658cc27fce21548a792d
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-01 22:47:30 +0300

    Updates

---
+++ precache-manifest.77f08e9fda27f74307d02d651c97269e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e9f614e7d03e2f8a72bd9429ff85ea",
+    "url": "/codenames/index.html"
+  },
+  {
+    "revision": "c848e3f3ee39372b2f00",
+    "url": "/codenames/static/css/main.36d66da8.chunk.css"
+  },
+  {
+    "revision": "2a9ff090e3405b5ba29b",
+    "url": "/codenames/static/js/2.45d9fd6a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/codenames/static/js/2.45d9fd6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c848e3f3ee39372b2f00",
+    "url": "/codenames/static/js/main.f26ada65.chunk.js"
+  },
+  {
+    "revision": "4caf5b2b468c6e8127f8",
+    "url": "/codenames/static/js/runtime-main.cb01a233.js"
+  },
+  {
+    "revision": "90595896067c8dca8953210ba731be3e",
+    "url": "/codenames/static/media/greece.90595896.svg"
+  },
+  {
+    "revision": "9d579f759224e6fec1a7fdc0fa56701e",
+    "url": "/codenames/static/media/main.9d579f75.png"
+  },
+  {
+    "revision": "e5564902e2642c5e6e2e98e68a7d41f5",
+    "url": "/codenames/static/media/uk.e5564902.svg"
+  }
+]);
