675545763e5664e19a0be6217ed937d6aa3916db
Aris Papadopoulos <aris.ppd@gmail.com>
2020-04-02 15:18:52 +0300

    App: Creates a wrapper div for game actions, fixes visual bug on firefox
    Signed-off-by: Aris Papadopoulos <aris.ppd@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -17,21 +17,22 @@ function App() {
                     <code style={{animation: `typing 2.5s steps(${texts[lang].title.length * 2}, end), blink-caret .75s step-end infinite`}}>{texts[lang].title}</code>
                 </div>
                 <img src={main} className="image-logo" alt="logo" />
-                <button className="create-game" onClick={() => createNewGame(lang)}>{texts[lang].newGame}</button>
-            <div className="language">
-                <p>{texts[lang].language}:</p>
-                <div>
-                    <button onClick={() => setLang('el')}><img className={(lang === 'el') ? 'active' : ''} src={gr} alt="greek flag"/></button>
-                    <button onClick={() => setLang('en')}><img className={(lang === 'en') ? 'active' : ''} src={uk} alt="uk flag"/></button>
+                <div className="actions">
+                    <button className="create-game" onClick={() => createNewGame(lang)}>{texts[lang].newGame}</button>
+                    <div className="language">
+                        <p>{texts[lang].language}:</p>
+                        <div>
+                            <button onClick={() => setLang('el')}><img className={(lang === 'el') ? 'active' : ''} src={gr} alt="greek flag"/></button>
+                            <button onClick={() => setLang('en')}><img className={(lang === 'en') ? 'active' : ''} src={uk} alt="uk flag"/></button>
+                        </div>
+                    </div>
                 </div>
-            </div>
             </header>
         </div>
     );
 }
 
 const createNewGame = (lang) => {
-    console.log(lang);
     const encodedGame = encodeGame(lang);
     history.push(`/game/${lang}/${encodedGame}`);
 }
